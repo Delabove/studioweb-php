dd0be1a1dffec85cd843ec05fef33811ffa6ed31
DeLayne LaBove <delayne.labove@gmail.com>
2021-02-09 20:49:37 -0600

    php basics defining variable

--- first-script.php
+++ first-script.php
@@ -8,7 +8,14 @@
 
     <?php
     print('this is some php code')
+    ?>
+    <?php
+    echo('this is the second code block.')
+    ?>
 
+    <?php
+    $_variable = "tasty ants.";
+    echo '<br>this is some php -1<br> '. $_variable;
     ?>
 </body>
 </html>
