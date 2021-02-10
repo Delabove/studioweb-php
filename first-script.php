1fde1ab50a492fdca9edc3c56c7867afdc8cf030
DeLayne LaBove <delayne.labove@gmail.com>
2021-02-09 21:30:18 -0600

    concatination and line breaks

--- first-script.php
+++ first-script.php
@@ -7,15 +7,20 @@
     <h3>Your first PHP code -amazing!</h3>
 
     <?php
-    echo('This is some php code.-1')
+    echo('This is some php code.-1 <br>')
     ?>
     <?php
-    echo('This is some php code.-2')
+    echo('This is some php code.-2 <br>')
     ?>
 
     <?php
     $_variable = "concatinated string.";
-    echo '<br>this is some php  '.  $_variable;
+    echo '<br>this is some php '.  $_variable .' <br>';
+    ?>
+
+    <?php
+    $_variable = "lives inside the string";
+    echo 'this is some php code ' . $_variable;
     ?>
 </body>
 </html>
