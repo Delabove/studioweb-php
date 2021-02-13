32cdef05b8823481b091bc3902e3b9e554b17c43
DeLayne LaBove <delayne.labove@gmail.com>
2021-02-13 07:22:28 -0600

    nav and style

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Ch 12 Advance PHP</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Calligraffitti&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="ch-12-styles.css">
+</head>
+<body>
+<nav>
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Protected Page</a></li>
+        <li><a href="">Contact</a></li>
+        <li><a href="">Log Out</a></li>
+    </ul>
+</nav>
+</body>
+</html>
