1217149d7591927d080187b7782ff2e17033df83
DeLayne LaBove <delayne.labove@gmail.com>
2021-02-12 21:00:21 -0600

    nav menu created with basic style

--- nav.php
+++ nav.php
@@ -3,15 +3,44 @@
 <head>
 
     <title>CSS Navigation</title>
+    <style>
+        /*.container{*/
+        /*    background-color: lavender;*/
+        /*}*/
+        ul {
+
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
+        li{
+            display: inline;
+            float: right;
+        }
+        li a {
+            display: block;
+            color: white;
+            font-size: 25px;
+            font-family: Didot;
+            font-weight: bolder;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+
+    </style>
 </head>
 <body>
+<div>
+    <ul  class="container">
+        <li  class="list-item"><a href="#home">Home</a></li>
+        <li  class="list-item"><a href="#about">About</a></li>
+        <li  class="list-item"><a href="#contact">Contact</a></li>
+    </ul>
+</div>
 
-<h1>CSS Navigation</h1>
-<ul>
-    <li>HOME</li>
-    <li>ABOUT</li>
-    <li>CONTACT</li>
-
-</ul>
 </body>
 </html>
