abf0ee2805b53f2fd6f2c5563939715fb32b1c78
DeLayne LaBove <delayne.labove@gmail.com>
2021-02-13 06:11:55 -0600

    custom function call

---
+++ custom-functions.php
@@ -0,0 +1,21 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="">
+<head>
+   <title>Chapter 10</title>
+</head>
+<body>
+<h1>Custom Functions</h1>
+
+<?php
+
+function firstFunction(){
+    echo "hello!";
+}
+firstFunction();
+
+?>
+
+
+</body>
+</html>
