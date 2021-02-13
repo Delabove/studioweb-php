3eb2cb1d3ce708ab3c3a1370e88b0b751e3415d4
DeLayne LaBove <delayne.labove@gmail.com>
2021-02-13 05:19:06 -0600

    formatting code to read

--- functions.php
+++ functions.php
@@ -1,14 +1,12 @@
 <?php
     $message= strlen("I am learning a little php;"); //counts the length of a strength
+    $message2 = date("D M Y");
+//        scandir("");
 
 ?>
 <!DOCTYPE html>
 <html lang="">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ch 8 Functions</title>
 </head>
 <body>
@@ -16,7 +14,7 @@
     <h1>PHP Functions</h1>
 
     <?php
-        echo "The length of the string is  " . $message;
+        echo print_r($message2);
     ?>
 
 </body>
