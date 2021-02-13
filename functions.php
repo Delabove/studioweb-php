985bf467df8dd951fc5554b63c785b9ac70ef48f
DeLayne LaBove <delayne.labove@gmail.com>
2021-02-13 04:55:20 -0600

    string length function

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+    $message= strlen("I am learning a little php;"); //counts the length of a strength
+
+?>
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ch 8 Functions</title>
+</head>
+<body>
+
+    <h1>PHP Functions</h1>
+
+    <?php
+        echo "The length of the string is  " . $message;
+    ?>
+
+</body>
+</html>
